69c0992f30ecccd4a5b8af89d2ddd8f361fabcff
thebenjaminibe <frankynoble@gmail.com>
2022-03-14 07:28:00 -0700

    My commit message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *My hello, printf task
+ *Return: return 1 after program execution
+ */
+int main(void)
+{
+printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+return (0);
+}

33a971e7cc718aaec76eb9fe330d554ba0ff554c
thebenjaminibe <frankynoble@gmail.com>
2022-03-15 09:42:21 -0700

    My commit message

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,9 +14,9 @@ for (base_16 = 48; base_16 <= 57; base_16++)
 putchar(base_16);
 }
 
-for 9base_16 = 97 ; base_16 <= 102; base_16++)
+for (base_16 = 97 ; base_16 <= 102; base_16++)
 {
-putchar(base_16)
+putchar(base_16);
 }
 
 putchar ('\n');

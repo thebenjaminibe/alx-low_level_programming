b09fcc9eba46bafee406e51e8ca4f6e29c21b7d7
thebenjaminibe <frankynoble@gmail.com>
2022-03-14 22:09:08 -0700

    My commit message

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,13 @@ int main(void)
 {
 char alphabet;
 
-for (alphabet = รร'a'; alphabet <= 'z'; alphabet++)
+for (alpahbet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 }
 
 putchar('\n');
 
-return(0);
+return (0);
 
 }

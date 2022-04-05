9d5a4774de5a2c4c12eee6a10033cd7a741cc78e
thebenjaminibe <frankynoble@gmail.com>
2022-04-05 09:21:03 -0700

    commit

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,10 +1,9 @@
+#include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-
-
-	printf("add(40, 2) = %d/n", add(40, 2));
-	printf("mul(40, 2) = %d/n", mul(40, 2));
-
+	printf("add(40, 2) = %d\n", add(40, 2));
+	printf("mul(40, 2) = %d\n", mul(40, 2));
 	return 0;
 }

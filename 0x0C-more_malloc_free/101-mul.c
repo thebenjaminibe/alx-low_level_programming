4ded7fe056eed83a9355e428adb0de54c20cd681
thebenjaminibe <frankynoble@gmail.com>
2022-04-12 12:54:00 -0700

    commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,29 +1,27 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+	char *f = argv[1];
+	char *s = argv[2];
 
+	if (argc != 3 || !onlyNumbers(f) || !onlyNumbers(s))
+	{
+		printf("Error\n");
+		exit(98);
 	}
-	mul = atol(argv[1]) *atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+	if (*f == 48 || *s == 48)
+		printf("0\n");
+	else
+		multiply(s, f);
+	return (0);
 }
